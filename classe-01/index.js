648e41d5510de31ea22003902805b40cfcda0a0e
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-06 04:43:50 -0300

    Filtro de caracteres

--- classe-01/index.js
+++ classe-01/index.js
@@ -1,1 +1,12 @@
 const cidades = ['Salvador', 'São Paulo', 'Brasilia', 'Recife', 'Rio de Janeiro'];
+
+const encontrados = cidades.filter((x) => {
+    if (x.length <= 8) {
+        return true;
+    } else {
+        return false
+    }
+});
+let alterastring = encontrados.join(", ");
+
+console.log(alterastring);
