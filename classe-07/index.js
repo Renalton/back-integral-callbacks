d84a65e9c5930dedda0d5a421f03dbf4257540fa
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-06 04:46:07 -0300

    Escrevendo em arquivos

--- classe-07/index.js
+++ classe-07/index.js
@@ -1,1 +1,12 @@
-const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+const  data = "Estou aprendendo JavaScript na Cubos Academy";
+
+const fs = require('fs');
+
+fs.writeFile("./classe-07/meuarquivo.txt", data, (err) =>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Arquivo gravado com sucesso!");
+    }
+});
+
