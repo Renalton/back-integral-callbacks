8f846947e2ef48a179601af148f8f5a61822cfb0
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-06 04:44:43 -0300

    Modificando um array

--- classe-03/index.js
+++ classe-03/index.js
@@ -1,1 +1,11 @@
 const frutas = ["Manga", "UVA", "abacaxi", "banaNA", "MAçã"];
+
+const newArray = frutas.map((x, index) => {
+
+    x = x.toLowerCase();
+
+    return index + ' - ' + x.replace(x.slice(0, 1), x.slice(0, 1).toUpperCase());
+
+});
+
+console.log(newArray);
