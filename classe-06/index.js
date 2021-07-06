8e5865858470fc370608694b8a8a2954da27e7df
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-06 04:45:50 -0300

    Temporizador de jogos

--- classe-06/index.js
+++ classe-06/index.js
@@ -1,1 +1,19 @@
 const jogadores = ['Guido', 'Dina', 'Léo', 'Nanda', 'Juninho'];
+let i = 0;
+
+console.log(`Cada jogador terá ${(1000 * (10 / jogadores.length)) /1000} segundo${(1000 * (10 / jogadores.length)) === 1? "" : "s"} para jogar`);
+function meusJogadores(){
+    
+    if(i < jogadores.length){
+        console.log(jogadores[i]);
+        i++;
+    }else{
+        console.log("Fim do jogo!");
+        clearInterval(intervalId);
+    }
+    
+}
+meusJogadores();
+const intervalId = setInterval(meusJogadores, (1000 * (10 / jogadores.length)));
+
+
