712c468c631140ee92639547776dff3d60541f90
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-07-06 04:44:24 -0300

    Busca de CEP

--- classe-02/index.js
+++ classe-02/index.js
@@ -4,3 +4,9 @@ const endereços = [
     { cep: 00222444, rua: "Avenida Paralela" },
     { cep: 11222333, rua: "Rua Carlos Gomes" },
 ];
+
+const meuCep = 00222444;
+
+const achou = endereços.find(x => x.cep === meuCep);
+
+console.log(achou.rua);
